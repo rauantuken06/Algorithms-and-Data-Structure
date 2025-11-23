736238b43dc4377e1989da976af7b6f580929f21
rauantuken06 <r_tuken@kbtu.kz>
2025-11-23 21:57:45 +0600

    lab10

---
+++ lab10/task_d.cpp
@@ -0,0 +1,115 @@
+#include <iostream>
+using namespace std;
+
+const int MAX_N = 5005;
+const int Q_SIZE = 200000;
+int adjacency[MAX_N][MAX_N];
+int n;
+
+struct Queue{
+    pair<int, int> tipo_queue[Q_SIZE];
+    int queue_left;
+    int queue_right;
+
+    Queue(){
+        queue_left = 0;
+        queue_right = 0;
+    }
+
+    void push(int vertex, int distance){
+        pair<int, int> for_put = make_pair(vertex, distance);
+        tipo_queue[queue_right] = for_put;
+        queue_right++; 
+    }
+
+    pair<int, int> front(){
+        return tipo_queue[queue_left];
+    }
+
+    void pop(){
+        queue_left++;
+    }
+
+    bool isEmpty(){
+        return queue_left == queue_right;
+    }
+};
+
+struct Graph{
+    bool color[MAX_N];
+
+    Graph(){
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                adjacency[i][j] = 0;
+            }
+        }
+    }
+
+    void add_edge(int a, int b){
+        adjacency[a][b] = 1;
+        adjacency[b][a] = 1;
+    }
+
+    void change_color_to_red(int vertex){
+        color[vertex] = true;
+    }
+
+    int bfs(int start){
+        if(color[start] == true){
+            return 0;
+        }
+
+        bool visited[n] = {false};
+        visited[start] = true;
+        Queue ochered;
+        ochered.push(start, 0);
+
+        while(!ochered.isEmpty()){
+            pair<int, int> current = ochered.front();
+            ochered.pop();
+            int current_node = current.first;
+            int current_dist = current.second;
+
+            for(int i = 0; i < n; i++){
+                if(adjacency[current_node][i] == 1 && visited[i] == false){
+                    visited[i] = true;
+                    if(color[i] == true){
+                        return current_dist + 1;
+                    }
+                    ochered.push(i, current_dist + 1);
+                }
+            }
+        }
+        return -1;
+    }
+};
+
+int main(){
+    int m, queries;
+    cin >> n >> m >> queries;
+
+    Graph graph;
+
+    for(int i = 0; i < m ; i++){
+        int a, b;
+        cin >> a >> b;
+        a--;
+        b--;
+        graph.add_edge(a, b);
+    }
+
+    while(queries--){
+        int command, vertex;
+        cin >> command >> vertex;
+        vertex--;
+        if(command == 1){
+            graph.change_color_to_red(vertex);
+        }
+        else if(command == 2){
+            cout << graph.bfs(vertex) << "\n";
+        }
+    }
+
+    return 0;
+}
